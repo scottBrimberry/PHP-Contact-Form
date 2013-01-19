28215c666507ba9764c5eac6a591d1c53d96ded9
Mohammed <me@mgakashim.com>
2013-01-19 18:00:39 +0000

    Added detailed documentation

--- mail.php
+++ mail.php
@@ -1,6 +1,8 @@
 <?php
+/* Developed by Moka, @ad7863, mgakashim.com, me@mgakashim.com */
+
 	require_once('recaptchalib.php');
-	  $privatekey = "your private key";
+	  $privatekey = "your private key"; // enter your private key here
 	  $resp = recaptcha_check_answer ($privatekey,
 	                                $_SERVER["REMOTE_ADDR"],
 	                                $_POST["recaptcha_challenge_field"],
@@ -8,34 +10,44 @@
 
   if (!$resp->is_valid) {
     // What happens when the CAPTCHA was entered incorrectly
-    echo "<script>javascript:Recaptcha.reload();</script>";
-    die("Sorry, the verification code wasn't entered correctly. Try again.");
+    echo "<script>javascript:Recaptcha.reload();</script>"; // reload captcha
+    die("Sorry, the verification code wasn't entered correctly. Try again."); // kill program and return error message
   } else {
 	  	if(isset($_POST['contactFormSubmitted'])) {
-		    // Form submission
+		    // Form submission. Feel free to add more , make sure you add validation and add them to the mail line.
 			$name = $_POST['name'];
 			$email = $_POST['email'];
 			$phone = $_POST['phone'];
 			$type = $_POST['type'];
 			$message = $_POST['message'];
-
+			
+			// check length of name, email and message
 			if (strlen($name) < 3 || strlen($email) < 8 || strlen($message) < 20) {
-				echo "<script>javascript:Recaptcha.reload();</script>";
-	  			exit("Please ensure you have completed all required fields.");
+				echo "<script>javascript:Recaptcha.reload();</script>"; // reload the captcha
+	  			exit("Please ensure you have completed all required fields."); // exit program, return message
 	  		}
-
+			
+			// validate email address
 	  		if (!(filter_var($email, FILTER_VALIDATE_EMAIL))) {
-	  			echo "<script>javascript:Recaptcha.reload();</script>";
-	  			exit("Please ensure you have entered a valid email address.");
+	  			echo "<script>javascript:Recaptcha.reload();</script>"; //reload the captcha
+	  			exit("Please ensure you have entered a valid email address."); // exit program, return message
 	  		}
-
+			
+			// Build form content
 			$formcontent=" From: $name \n Phone: $phone \n Type: $type \n Message: $message";
+			// message should get wordwrapped after 70 chracters? Words?
 			$message = wordwrap($formcontent, 70, "\r\n");
-			$recipient = "me@mgakashim.com";
+			// Enter your email address
+			$recipient = "your email address";
+			// Enter a subject, only you will see this so make it useful
 			$subject = "PHAYSE $name for $type";
+			// 'From' mail header
 			$mailheader = "From: $email \r\n";
+			// Send email, if something goes wrong, kill programm and return error message
 			mail($recipient, $subject, $message, $mailheader) or die("Something went wrong, please try again or contact us via email instead: contact [at] phayse [dot] com.");
+			// If all's well, return success message
 			echo "Thank You! Your message has been sent.";
+			// ...and clear the message box
 			echo "<script>$('#inputMessage').val('');</script>";
 	  	}
   }
